26269fcd5cd3a131197a455719092698df89025f
Joshua Azicate <jayazicate@gmail.com>
2019-01-29 20:59:48 -0800

    Update functions.test.js

--- js/functions.test.js
+++ js/functions.test.js
@@ -4,10 +4,7 @@ test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-test('adds 4 + 5 to equal 9', () => {
-  expect(sum(4, 5)).toBe(9);
+test('adds 7 + 3 to equal 10', () => {
+  expect(sum(7, 3)).toBe(9);
 });
 
-test('adds 5 + 5 to equal 10', () => {
-  expect(sum(5, 5)).toBe(10);
-});
